0cafaae2f42c2522e66acc83bcb2ca588b7b0dc8
raphael <raphael.birkl@gmail.com>
2024-01-16 11:27:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather_app_asynchronous
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/weather_app_asynchronous/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weather_app_asynchronous
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
