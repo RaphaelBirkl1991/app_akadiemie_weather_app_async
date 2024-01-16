8965b339fb537451074f3e443ad1162f04a2c6ac
raphael <raphael.birkl@gmail.com>
2024-01-16 11:55:10 +0100

    task 1 done

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,45 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Wetter-App"),
+          backgroundColor: Colors.lightBlue,
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            children: [
+              const Spacer(),
+              const Spacer(),
+              const Spacer(),
+              const Spacer(),
+              const Text(
+                "Stadt: Stuttgart",
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue),
+              ),
+              const Spacer(),
+              const Text("Gefühlte Temperatur: -10°"),
+              const Spacer(),
+              const Text("Temperatur: -4°"),
+              const Spacer(),
+              const Text("Niederschlag: 15.00mm"),
+              const Spacer(),
+              const Text("Tageszeit: Tag"),
+              const Spacer(),
+              const Text("Standort: lat: 48.783, long: 9.183"),
+              const Spacer(),
+              ElevatedButton(
+                  onPressed: () {}, child: const Text("Vorhersage überprüfen")),
+              const Spacer(),
+              const Spacer(),
+              const Spacer(),
+              const Spacer(),
+            ],
+          ),
         ),
       ),
     );
